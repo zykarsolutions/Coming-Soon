df9a3df65a971f2a8b0424f8b72323ea013394d5
Moses Mwila <moseshb17@gmail.com>
2023-02-08 02:58:42 +0200

    Intial Deployment

---
+++ build/precache-manifest.fe3b82c4f8819b552528c213c140ad1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c73ef6d1a42ba6b5737d",
+    "url": "/static/css/main.bf891058.chunk.css"
+  },
+  {
+    "revision": "c73ef6d1a42ba6b5737d",
+    "url": "/static/js/main.c73ef6d1.chunk.js"
+  },
+  {
+    "revision": "a6bf9075d2fd845ee1c2",
+    "url": "/static/js/1.a6bf9075.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4454e048ceda5db506eec76e37c6fea3",
+    "url": "/index.html"
+  }
+];
